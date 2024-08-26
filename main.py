9566ddb2a6fa83a912bfdaefbfaf5975cae0cfdb
Leo Rutten <lrutten@users.noreply.github.com>
2024-08-26 16:44:17 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,3 @@
+def on_forever():
+    TobbieII.forward()
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    TobbieII.forward()
+    basic.pause(3000)
+    TobbieII.stopwalk()
+    basic.pause(3000)
+    TobbieII.rightward()
+    basic.pause(800)
+    TobbieII.stopturn()
+    basic.pause(1000)
+})
